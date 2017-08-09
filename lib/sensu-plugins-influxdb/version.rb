505f83d1ff9b9bbb70c4d5022f179e2a5dd556d7
Peter Hoeg <peter@hoeg.com>
2017-08-09 14:38:10 +0800

    fe

--- lib/sensu-plugins-influxdb/version.rb
+++ lib/sensu-plugins-influxdb/version.rb
@@ -1,9 +1,7 @@
 module SensuPluginsInfluxdb
-  module Version
-    MAJOR = 1
-    MINOR = 2
-    PATCH = 0
+  MAJOR = 1
+  MINOR = 2
+  PATCH = 0
 
-    VER_STRING = [MAJOR, MINOR, PATCH].compact.join('.')
-  end
+  VERSION = [MAJOR, MINOR, PATCH].compact.join('.')
 end
